238dfbc4e6dbacfd3d8d5d3761810a9d8d24b10c
MadRoosterCro <dario.car.kz@gmail.com>
2022-03-10 16:22:26 +0100

    Adding markdown toggle

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+
+export const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
+    </div>
+  );
+};
