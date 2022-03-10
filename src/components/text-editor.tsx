2413b3ad5a8de7539109f9738bd6ac1e45d862f5
MadRoosterCro <dario.car.kz@gmail.com>
2022-03-10 16:31:52 +0100

    Adding togle for markdown editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,12 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 export const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
 
   useEffect(() => {
-    const listener = () => {
-      setEditing(false);
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      } else {
+        setEditing(false);
+      }
     };
     document.addEventListener('click', listener, { capture: true });
 
@@ -17,7 +26,7 @@ export const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div>
+      <div ref={ref}>
         <MDEditor />
       </div>
     );
