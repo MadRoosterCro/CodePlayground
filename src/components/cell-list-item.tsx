83cd8d602eecf320fb8bc07519b249805be435df
MadRoosterCro <dario.car.kz@gmail.com>
2022-03-17 11:50:34 +0100

    Action bar init

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,7 +1,7 @@
 import { Cell } from '../state';
 import { CodeCell } from './code-cell';
 import { TextEditor } from './text-editor';
-
+import { ActionBar } from './action-bar';
 interface CellListItemProps {
   cell: Cell;
 }
@@ -13,5 +13,10 @@ export const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   } else {
     child = <TextEditor cell={cell} />;
   }
-  return <div>{child}</div>;
+  return (
+    <div>
+      <ActionBar id={cell.id} />
+      {child}
+    </div>
+  );
 };
