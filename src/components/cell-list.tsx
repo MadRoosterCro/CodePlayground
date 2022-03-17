d5e6e88ca43c2d4df90d017180a40d8d1ba5354e
MadRoosterCro <dario.car.kz@gmail.com>
2022-03-17 12:21:34 +0100

    Rendering AddCell

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,6 @@
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import { CellListItem } from './cell-list-item';
+import { AddCell } from './add-cell';
 
 export const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -7,7 +8,15 @@ export const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem key={cell.id} cell={cell} />
+    </>
   ));
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
--- src/state/store.ts
+++ src/state/store.ts
@@ -9,13 +9,6 @@ store.dispatch({
   type: ActionType.INSERT_CELL_BEFORE,
   payload: {
     id: null,
-    type: 'code',
-  },
-});
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
     type: 'text',
   },
 });
