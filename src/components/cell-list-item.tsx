1bd534466de93d59bb813767a8d4eda506b0766a
MadRoosterCro <dario.car.kz@gmail.com>
2022-03-17 12:13:19 +0100

    AppBar dynamic opacity

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,4 +1,4 @@
-import './cell-list-item.css'
+import './cell-list-item.css';
 import { Cell } from '../state';
 import { CodeCell } from './code-cell';
 import { TextEditor } from './text-editor';
@@ -10,14 +10,21 @@ interface CellListItemProps {
 export const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === 'code') {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <>
+        <div className={'action-bar-wrapper'}>
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
   }
-  return (
-    <div className={'cell-list-item'}>
-      {child}
-      <ActionBar id={cell.id} />
-    </div>
-  );
+  return <div className={'cell-list-item'}>{child}</div>;
 };
--- src/state/store.ts
+++ src/state/store.ts
@@ -9,6 +9,13 @@ store.dispatch({
   type: ActionType.INSERT_CELL_BEFORE,
   payload: {
     id: null,
+    type: 'code',
+  },
+});
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
     type: 'text',
   },
 });
